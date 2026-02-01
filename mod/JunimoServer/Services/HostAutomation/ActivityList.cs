03e24d3893d181bfc61d0d8804ac671bc2a3558e
Julian Vallée <julian@vallee-design.de>
2026-02-01 20:13:15 +0100

    chore: fix code formatting

--- mod/JunimoServer/Services/HostAutomation/ActivityList.cs
+++ mod/JunimoServer/Services/HostAutomation/ActivityList.cs
@@ -4,60 +4,60 @@ using System.Collections.Generic;
 
 namespace JunimoServer.Services.HostAutomation
 {
-  public class ActivityList : List<Activity>
-  {
-    private IModHelper _helper;
-
-    #pragma warning disable CS0169 // Ignore unused field `_activities`
-    private List<Activity> _activities;
-    #pragma warning restore CS0169
-
-    public ActivityList(IModHelper helper, IMonitor monitor)
+    public class ActivityList : List<Activity>
     {
-      _helper = helper;
+        private IModHelper _helper;
 
-      _helper.Events.GameLoop.SaveLoaded += EnableAll;
-      _helper.Events.GameLoop.DayStarted += DayStartAll;
-      _helper.Events.GameLoop.UpdateTicked += TickAll;
+#pragma warning disable CS0169 // Ignore unused field `_activities`
+        private List<Activity> _activities;
+#pragma warning restore CS0169
+
+        public ActivityList(IModHelper helper, IMonitor monitor)
+        {
+            _helper = helper;
+
+            _helper.Events.GameLoop.SaveLoaded += EnableAll;
+            _helper.Events.GameLoop.DayStarted += DayStartAll;
+            _helper.Events.GameLoop.UpdateTicked += TickAll;
+        }
+
+        public void EnableAll(object sender, SaveLoadedEventArgs e)
+        {
+            foreach (var activity in this)
+            {
+                activity.Enable();
+            }
+        }
+
+        // public void DisableAll()
+        // {
+        //   foreach (var activity in this)
+        //   {
+        //     activity.Disable();
+        //   }
+        // }
+
+        public void TickAll(object sender, UpdateTickedEventArgs e)
+        {
+            foreach (var activity in this)
+            {
+                activity.HandleTick();
+            }
+        }
+
+        public void DayStartAll(object sender, DayStartedEventArgs e)
+        {
+            foreach (var activity in this)
+            {
+                activity.HandleDayStart();
+            }
+        }
+
+        ~ActivityList()
+        {
+            _helper.Events.GameLoop.SaveLoaded -= EnableAll;
+            _helper.Events.GameLoop.DayStarted -= DayStartAll;
+            _helper.Events.GameLoop.UpdateTicked -= TickAll;
+        }
     }
-
-    public void EnableAll(object sender, SaveLoadedEventArgs e)
-    {
-      foreach (var activity in this)
-      {
-        activity.Enable();
-      }
-    }
-
-    // public void DisableAll()
-    // {
-    //   foreach (var activity in this)
-    //   {
-    //     activity.Disable();
-    //   }
-    // }
-
-    public void TickAll(object sender, UpdateTickedEventArgs e)
-    {
-      foreach (var activity in this)
-      {
-        activity.HandleTick();
-      }
-    }
-
-    public void DayStartAll(object sender, DayStartedEventArgs e)
-    {
-      foreach (var activity in this)
-      {
-        activity.HandleDayStart();
-      }
-    }
-
-    ~ActivityList()
-    {
-      _helper.Events.GameLoop.SaveLoaded -= EnableAll;
-      _helper.Events.GameLoop.DayStarted -= DayStartAll;
-      _helper.Events.GameLoop.UpdateTicked -= TickAll;
-    }
-  }
 }
